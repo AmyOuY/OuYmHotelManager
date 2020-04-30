06d78b40ab1be7f2e71d2a703901ef9c0fdf8ac3
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-04-29 23:02:05 -0400

    Wire Up Login View Model

--- OHMDesktopUI/Bootstrapper.cs
+++ OHMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using OHMDesktopUI.Helpers;
 using OHMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace OHMDesktopUI
 {
@@ -16,6 +18,11 @@ namespace OHMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
 
---
+++ OHMDesktopUI/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OHMDesktopUI.Helpers
+{
+    public class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached(
+                "BoundedPassword",
+                typeof(string),
+                typeof(PasswordBoxHelper),
+                new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+            var box = d as PasswordBox;
+
+            if (box != null)
+            {
+                box.PasswordChanged -= PasswordChanged;
+                box.PasswordChanged += PasswordChanged;
+            }
+
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            if (string.Equals(GetBoundPassword(d), value))
+            {
+                return;
+            }
+
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var box = d as PasswordBox;
+
+            if (box == null)
+            {
+                return;
+            }
+
+            box.Password = GetBoundPassword(d);
+        } 
+
+
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox password = sender as PasswordBox;
+
+            SetBoundPassword(password, password.Password);
+
+            password.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(password, new Object[] { password.Password.Length, 0 });
+        }
+    }
+}
