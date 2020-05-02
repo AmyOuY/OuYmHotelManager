e3a65692087f93eef9fef1dd41e8d0bd935a7039
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-05-01 23:09:35 -0400

    Create API User Endpoint
    Add User endpoint to API for accessing data from sql database.

---
+++ OHMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,22 @@
+﻿using OHMDataManager.Library.Internal.DataAccess;
+using OHMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public UserModel GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookUp", new { Id }, "OHMData").FirstOrDefault();
+
+            return output;
+        }
+    }
+}
---
+++ OHMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using OHMDataManager.Library.DataAccess;
+using OHMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OHMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public UserModel GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
